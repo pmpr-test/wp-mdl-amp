043e22cb955ebbd2d8ae68e5814f5eb33d0c00f5
pmpr.dev <pmpr.dev@gmail.com>
2024-09-14 20:04:52 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66e5e78ceff1c             |
+    |             66e5ec44320c4             |
     |_______________________________________|
 */
- use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\151\163\137\141\155\x70")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\x69\163\137\141\155\160\137\x65\156\144\160\x6f\x69\x6e\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
+ use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists("\x69\x73\137\141\155\160")) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists("\151\163\137\141\x6d\x70\x5f\145\156\x64\160\157\x69\x6e\x74")) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- src/Pmpr/Module/AMP/Container.php
+++ src/Pmpr/Module/AMP/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66e5e78ceff1c             |
+    |             66e5ec44320c4             |
     |_______________________________________|
 */
- namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\60\x2e\61"; const wcwemgogyesywcww = "\150\164\x74\160\x73\x3a\x2f\57\143\144\156\x2e\x61\155\160\160\162\x6f\152\x65\143\x74\x2e\157\162\147\57\x76\x30"; use CommonTrait; public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } }
+ namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\x30\x2e\x31"; const wcwemgogyesywcww = "\x68\164\164\x70\163\72\x2f\57\143\x64\x6e\56\x61\x6d\160\x70\x72\x6f\x6a\145\x63\x74\56\157\162\x67\57\x76\60"; use CommonTrait; public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } }
