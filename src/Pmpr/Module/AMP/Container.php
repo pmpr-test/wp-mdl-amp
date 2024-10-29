42358a5cd168078024dac94b3642a7760f7fc6e6
pmpr.dev <pmpr.dev@gmail.com>
2024-10-29 10:21:02 +0000

    Obfuscated

--- src/Pmpr/Module/AMP/Container.php
+++ src/Pmpr/Module/AMP/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d97ef2c6c3             |
+    |             6720b6ef23c92             |
     |_______________________________________|
 */
- namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\60\56\61"; const wcwemgogyesywcww = "\150\x74\164\160\x73\72\x2f\x2f\x63\144\x6e\x2e\x61\x6d\160\x70\162\x6f\152\x65\x63\x74\x2e\x6f\x72\x67\x2f\166\60"; use CommonTrait; }
+ namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = "\60\56\61"; const wcwemgogyesywcww = "\150\164\x74\x70\x73\72\x2f\57\143\144\x6e\x2e\141\x6d\x70\x70\x72\157\x6a\x65\x63\x74\x2e\157\x72\x67\x2f\166\60"; use CommonTrait; }
--- template/php/header.php
+++ template/php/header.php
@@ -1,16 +1,16 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d97ef2c6c3             |
+    |             6720b6ef23c92             |
     |_______________________________________|
 */
 ype html>
 <html amp ⚡ <?php  language_attributes(); ?>>
 <head>
-	<meta charset="<?php  bloginfo("\x63\150\x61\x72\x73\145\x74"); ?>" />
+	<meta charset="<?php  bloginfo("\x63\x68\x61\162\x73\x65\164"); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php  do_action("\141\x6d\x70\x5f\167\x70\137\150\x65\x61\x64"); ?>
+	<?php  do_action("\x61\x6d\160\x5f\x77\160\x5f\150\x65\x61\x64"); ?>
 </head>
 <body <?php  body_class(); ?>>
-<?php  pmpr_do_action("\141\155\x70\x5f\x62\157\144\x79\137\157\x70\x65\x6e");
+<?php  pmpr_do_action("\141\x6d\x70\137\x62\x6f\x64\171\137\x6f\160\x65\156");
