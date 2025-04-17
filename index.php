f4d2f6364ba9769880a76157a94c36a42f23b69f
pmpr.dev <pmpr.dev@gmail.com>
2025-04-17 12:49:21 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6800f7007ebd2             |
+    |             6800f8aa54a7b             |
     |_______________________________________|
 */
  use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists('is_amp')) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists('is_amp_endpoint')) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
