29af579b03534a19cbfad4ea73a4ea5fd66e1ac3
pmpr.dev <pmpr.dev@gmail.com>
2025-12-24 19:43:58 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6800f8aa54a7b             |
+    |             694c4255df416             |
     |_______________________________________|
 */
  use Pmpr\Module\AMP\AMP; AMP::symcgieuakksimmu(); if (!function_exists('is_amp')) { function is_amp() { return AMP::symcgieuakksimmu()->oywyqcgumoecwoga(); } } if (!function_exists('is_amp_endpoint')) { function is_amp_endpoint() { return AMP::symcgieuakksimmu()->smowememmgeukwki(); } }
--- src/Pmpr/Module/AMP/Container.php
+++ src/Pmpr/Module/AMP/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6800f8aa54a7b             |
+    |             694c4255df416             |
     |_______________________________________|
 */
  namespace Pmpr\Module\AMP; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Module\AMP\Traits\CommonTrait; abstract class Container extends BaseClass { const qgwkyemuiussyyoy = '0.1'; const wcwemgogyesywcww = 'https://cdn.ampproject.org/v0'; use CommonTrait; }
